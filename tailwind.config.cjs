b8b0e8a37f65a78a4936e22f5d8594052521d45d
Prakash Raman <prakash.raman.ka@gmail.com>
2023-02-04 13:46:11 -0500

    grid template

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#fcfcfd",
+        line: "#f4f5f6",
+      },
+    },
   },
   plugins: [],
 };
